ff31c749978551bbfd89b4679ab6e513a2d27ddd
231499w <2314993w@student.gla.ac.uk>
2020-11-10 10:22:57 +0000

    postgres database working

--- experiment_portal_project/settings.py
+++ experiment_portal_project/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'experiment_portal_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'db_admin',
+        'PASSWORD': 'Delete44',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
